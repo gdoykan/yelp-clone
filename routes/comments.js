a991672ffbbebb80c5fcfd142e7dd3e6472133e5
George Doykan <gydoykan@gmail.com>
2017-08-03 19:25:02 +0000

    can delete comments now

--- routes/comments.js
+++ routes/comments.js
@@ -74,4 +74,16 @@ router.put("/:comment_id", function(req, res){
     })
 })
 
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id", function(req, res){
+   //findByIdAndRemove
+   Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else{
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   })
+});
+
 module.exports = router;
