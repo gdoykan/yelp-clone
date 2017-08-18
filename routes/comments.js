3541406db7414d321e039a7b484e090f42659a80
George Doykan <gydoykan@gmail.com>
2017-08-18 20:50:44 +0000

    more comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -53,7 +53,7 @@ function isLoggedIn(req, res, next){
 }
 
 //Comments edit route
-router.get("/:comment_id/edit", function(req,res){
+router.get("/:comment_id/edit", checkCommentOwnership,function(req,res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect("back");
@@ -64,7 +64,7 @@ router.get("/:comment_id/edit", function(req,res){
 });
 
 //Comment Update
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership,function(req, res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err){
             res.redirect("back");
@@ -75,7 +75,7 @@ router.put("/:comment_id", function(req, res){
 })
 
 //COMMENT DESTROY ROUTE
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership,function(req, res){
    //findByIdAndRemove
    Comment.findByIdAndRemove(req.params.comment_id, function(err){
        if(err){
@@ -85,5 +85,24 @@ router.delete("/:comment_id", function(req, res){
        }
    })
 });
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+            Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back")
+            } else{
+                //does user own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else{
+                    res.redirect("back");
+                }
+            }
+            });
+    }  else{
+            res.redirect("back"); //previous page
+    }
+}
 
 module.exports = router;
